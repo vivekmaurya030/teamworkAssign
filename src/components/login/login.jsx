27424b5629990961a51a3e54a5c3268eef5fd51f
Vivek Maurya <vivekmaurya030gmail.com>
2023-05-27 00:12:30 +0530

    login

---
+++ src/components/login/login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './login.scss'
+
+const LogIn =()=>{
+    return(
+        <div className="login">
+            <div className="login-header">
+                <h1>KLARA MOON</h1>
+                <p>cleaning service providing <p className="cmp">Company</p></p>
+            </div>
+            <div className="login-body">
+                
+            </div>
+        </div>
+    )
+}
+
+export default LogIn;
