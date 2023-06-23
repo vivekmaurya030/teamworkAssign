be7868ede2f356ae000b43766c45df7392375ac9
Vivek Maurya <vivekmaurya030gmail.com>
2023-06-23 10:19:14 +0530

    footer changes

--- src/pages/faqsPage.js
+++ src/pages/faqsPage.js
@@ -1,11 +1,13 @@
 import Header from "../components/header";
 import FAQPage from "../components/faqs/faqs";
+import Footers from "../components/footer/footers";
 
 const LogInPage=()=>{
     return(
         <>
             <Header /> 
            <FAQPage/>
+           <Footers/>
         </>
     )
 }
