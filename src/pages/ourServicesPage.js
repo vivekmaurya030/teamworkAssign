ce0ae1e4ec482d1bb1d738476da23107b15ea5ff
Vivek Maurya <vivekmaurya030gmail.com>
2023-07-05 16:35:25 +0530

    services page

--- src/pages/ourServicesPage.js
+++ src/pages/ourServicesPage.js
@@ -1,14 +1,14 @@
 import React from "react";
-import OurServices from "../components/ourServices/ourServices";
 import Header from "../components/header";
 import Footers from "../components/footer/footers";
+import OurServices from "../components/ourServices/ourServices";
 
 const OurServicesPage=()=>{
     return(
         <div>
-            <Header/>
-            <OurServices/>
-            <Footers/>
+            <Header />
+            <OurServices />
+            <Footers />
         </div>
     )
 }
